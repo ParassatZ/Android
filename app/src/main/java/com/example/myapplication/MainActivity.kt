126630a9075cd2761851fb1fb51b2d31893c23dd
ParassatZ <krayx170804@gmail.com>
2023-10-31 17:44:12 +0600

    hw3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,30 +1,25 @@
 package com.example.myapplication
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var colorAdapter: ColorAdapter
-    private val colorList = listOf(
-        ColorItem(Color.RED, "Red"),
-        ColorItem(Color.GREEN, "Green"),
-        ColorItem(Color.BLUE, "Blue"),
-        ColorItem(Color.YELLOW, "Yellow"),
-        ColorItem(Color.MAGENTA, "Magenta")
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        colorAdapter = ColorAdapter(colorList)
-        recyclerView.adapter = colorAdapter
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+
+        }
     }
 }
+
