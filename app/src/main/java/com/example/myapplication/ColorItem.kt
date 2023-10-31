c22ad2a88c70101c50c9c1c844dd1477da0c877c
ParassatZ <krayx170804@gmail.com>
2023-10-31 17:13:19 +0600

    hw3

---
+++ app/src/main/java/com/example/myapplication/ColorItem.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class ColorItem(val color: Int, val name: String)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var colorAdapter: ColorAdapter
+    private val colorList = listOf(
+        ColorItem(Color.RED, "Red"),
+        ColorItem(Color.GREEN, "Green"),
+        ColorItem(Color.BLUE, "Blue"),
+        ColorItem(Color.YELLOW, "Yellow"),
+        ColorItem(Color.MAGENTA, "Magenta")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        colorAdapter = ColorAdapter(colorList)
+        recyclerView.adapter = colorAdapter
     }
 }
